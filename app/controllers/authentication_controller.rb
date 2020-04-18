d61094a3f5e920da3cf490feba0b323cc5438255
Efe <knowledgeagare157@gmail.com>
2020-04-18 12:07:36 +0100

    create the controller action that logs the user in

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,9 +1,5 @@
 class AuthenticationController < ApplicationController
 
-
-  def index
-  end
-
   def register_user
     @user = Validations::RegisterUser.new(user_params)
  
@@ -19,12 +15,33 @@ class AuthenticationController < ApplicationController
     end
   end
 
+  def login_user
+      @user = Validations::RegisterUser.new(login_user_params)
+  
+      if !@user.valid?
+        json_response(@user.errors, :bad_request)
+      else
+        response = create_user(user_params)
+        if response['message'] != 'Success'
+          json_response(response)
+        else
+
+        end
+      end
+  end
+
   private
 
   def user_params 
     {
         first_name: params[:last_name],
         last_name: params[:last_name],
+        email: params[:email],
+        password: params[:password]
+    }
+  end
+  def login_user_params 
+    {
         email: params[:email],
         password: params[:password]
     }
