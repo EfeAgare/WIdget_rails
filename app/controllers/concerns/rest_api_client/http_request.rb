66bb494222b3138a597e747b2bfa6be8e6ccc4d9
Efe <knowledgeagare157@gmail.com>
2020-04-19 08:47:05 +0100

    user me identifier search

--- app/controllers/concerns/rest_api_client/http_request.rb
+++ app/controllers/concerns/rest_api_client/http_request.rb
@@ -73,5 +73,12 @@ module RestApiClient
   
       JSON.parse(resData)
     end
+
+    def widget_me_search
+      resData = RestClient::Request.execute(method: :get, url: "#{BASE_URL}/api/v1/users/me/widgets?client_id=#{ENV["client_id"]}&client_secret=#{ENV["client_secret"]}",
+        headers: {'content-type': 'application/json', Authorization:  "Bearer #{session[:token]}"} )  {|response, request, result| response }
+  
+      JSON.parse(resData)
+    end
   end
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,18 @@
 class SearchController < ApplicationController
 
   def widget_search
-    @widgets = general_search
+    if params[:y] == 'All' 
+      @widgets = general_search
+    elsif params[:y] == 'Me'
+      @widgets = me_widget_search
+    end
   end
 
   def general_search
     search(params[:term])['data']['widgets']
+  end
+
+  def me_widget_search
+    widget_me_search['data']['widgets']
   end  
 end
