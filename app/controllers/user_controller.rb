6f847eaab0c3ab221b98e4bb1d9a03d83bef5652
Efe <knowledgeagare157@gmail.com>
2020-04-19 12:09:29 +0100

    enable user id and term search in user profile page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,12 +1,20 @@
 class UserController < ApplicationController
   def index
     @user = get_current_user_profile['data']['user']
+    @widgets = get_all_user_widget['data']['widgets']
+
   end
 
   def other_users_profile
     @user = get_other_users_profile(params[:id])
+
     if @user['data'].present?
       @user = @user['data']['user']
+      if params[:term].present?
+        @widgets = user_id_and_term_params_widget_search
+      else
+        @widgets = user_id_params_widget_search
+      end
     else
       render_404
     end
@@ -14,15 +22,15 @@ class UserController < ApplicationController
 
 
   def user_widget_search
-    if params[:term].present?
-
-    else
-     @widgets = user_id_with_params_widget_search
-    end
-    render partial: 'user/user_widget_search_result', locals: { widgets: @widgets }
+    other_users_profile
+    render template: 'user/index'
   end
 
-  def user_id_with_params_widget_search
-   widget_user_id_search(params[:Id])['data']['widgets']
+  def user_id_params_widget_search
+    widget_user_id_search(params[:id])['data']['widgets']
+  end
+
+  def user_id_and_term_params_widget_search
+    widget_user_id_and_term_search(params[:id], params[:term])['data']['widgets']
   end
 end
